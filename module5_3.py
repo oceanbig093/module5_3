d019ca6a9ff51b0ec105559e3e94603d9a9f2a61
oceanbig093 <innervision41@mail.ru>
2024-09-13 16:53:57 +0300

    Add files via upload

---
+++ module5_3.py
@@ -0,0 +1,76 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, количество этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors >= other
+
+    def __le__(self, other):
+        return self.__eq__(other) or self.__lt__(other)
+
+    def __gt__(self, other):
+        return not self.__le__(other)
+
+    def __add__(self, other):
+        if isinstance(other, House):
+            self.number_of_floors += other.number_of_floors
+        if isinstance(other, int):
+            self.number_of_floors += other
+        return self
+
+    def __iadd__(self, other):
+        return self.__add__(other)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+
+h1 = House('ЖК Малиновка', 15)
+h2 = House('ЖК Смородиновка', 7)
+print(h1)
+print(h2)
+print(h1 == h2)
+h1 = h1 + 10  # add
+print(h1)
+print(h1 == h2)
+h1 += 10  # iadd
+print(h1)
+h2 = 10 + h2  # radd
+print(h2)
+print(h1 > h2)  # gt
+print(h1 >= h2)  # ge
+print(h1 < h2)  # lt
+print(h1 <= h2)  # le
+print(h1 != h2)  # ne
